65f22c7a61531e519956bfd6a15773cc4df9014c
px1994 <pritesh13590@gmail.com>
2026-03-02 22:37:39 +0530

    added data_prep.py

---
+++ src/data/data_prep.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import pandas as pd
+
+def load_data(filepath: str):
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f'Error loading data from{filepath}:{e}')
+    
+#train_data = pd.read_csv('./data/raw/train.csv')
+#test_data = pd.read_csv('./data/raw/test.csv')
+
+def fill_missing_with_median(df):
+    try:
+        for column in df.columns:
+            if df[column].isnull().any():
+                median_value = df[column].median()
+                df[column].fillna(median_value,inplace=True)
+        return df
+    except Exception as e:
+        raise(f'Error Filling missing values:{e}')
+
+
+def save_data(df: pd.DataFrame,filepath: str) -> None:
+    try:
+        df.to_csv(filepath, index=False)
+    except Exception as e:
+        raise Exception(f'Error Saving Data to{filepath}:{e}')
+
+def main():
+    raw_data_path = './data/raw' # loading data from data collection
+    processed_data_path = './data/processed' # after processing saving data 
+ 
+    train_data = load_data(os.path.join(raw_data_path,'train.csv'))
+    test_data = load_data(os.path.join(raw_data_path,'test.csv'))
+
+    train_processed_data = fill_missing_with_median(train_data)
+    test_processed_data = fill_missing_with_median(test_data)
+
+    os.makedirs(processed_data_path)
+
+    save_data(train_processed_data, os.path.join(processed_data_path,'train_processed.csv'))
+    save_data(test_processed_data, os.path.join(processed_data_path,'test_processed.csv'))
+
+
+#train_processed_data = fill_missing_with_median(train_data)
+#test_processed_data = fill_missing_with_median(test_data)
+
+
+#data_path = os.path.join('data','processed')
+#os.makedirs(data_path)
+
+#train_processed_data.to_csv(os.path.join(data_path,'train_processed.csv'),index=False)
+#test_processed_data.to_csv(os.path.join(data_path,'test_processed.csv'),index=False)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
